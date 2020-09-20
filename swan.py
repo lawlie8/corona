1daa3faa986c97f338f375b6c49bb458b7f8dfe4
lawlie8 <50700194+lawlie8@users.noreply.github.com>
2020-09-20 16:08:22 +0530

    'null'

--- swan.py
+++
@@ -1,5 +0,0 @@
-import requests
-payload = {'email':'are_you_hiring_interns@protonmail.com'}
-for i in range(0,1000):
-    r = requests.post('http://localhost/DBMS/index.php',data=payload)
-    print(i)
